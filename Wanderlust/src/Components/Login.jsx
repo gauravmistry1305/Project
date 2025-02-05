6bb86fd879ddc5f3483d46c34a3d8ac9e84be773
gauravmistry1305 <mistrypc29@gmail.com>
2025-02-05 22:54:39 +0530

    commit

--- Wanderlust/src/Components/Login.jsx
+++ Wanderlust/src/Components/Login.jsx
@@ -1,49 +1,48 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
-     const [password, setPassword] = useState('');
-     const navigate = useNavigate(); 
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-         alert('Login Successful');
-         navigate('/home');
+        alert('Login Successful');
+        navigate('/home');
         // Redirect logic can be added here
-     };
+    };
 
     return (
-        <div className="login">
+        <div className="login-form">
             <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
 
                 <label>Email:</label>
-                <input 
-                    type="email" 
-                    name="email" 
-                    id="email" 
-                    value={email} 
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    required 
-                />
+                    required />
                 <br />
 
                 <label>Password:</label>
-                <input 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    value={password} 
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    required 
+                    required
                 />
                 <br />
 
                 <button type="submit">Login</button>
             </form>
-            </div>
+        </div>
     );
 };
 
